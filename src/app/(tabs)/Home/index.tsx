b49b3bd7c9af43aedfe095e2617ebbabac45b060
Paulo Henrique <100245785+PHenrique01@users.noreply.github.com>
2025-06-01 11:36:39 -0300

    Update: Adding the components

--- src/app/(tabs)/Home/index.tsx
+++ src/app/(tabs)/Home/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StatusBar, Text, TextInput, View } from "react-native";
+import Constants  from "expo-constants";
 
 export default function Home(){
+    const statusBarHeight = Constants.statusBarHeight;
+
     return(
-        <View>
-            <Text>Página home</Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-white">
+            <StatusBar barStyle={"dark-content"}/>
+            <ScrollView style={{ marginTop: statusBarHeight + 8}} showsVerticalScrollIndicator={false} className="flex-1 px-4">
+                <View className="flex gap-10 pb-4">
+                    <View className="flex-1">
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
